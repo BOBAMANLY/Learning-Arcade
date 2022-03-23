467b3aaef037b45f7ba540610fc120b406ad92d8
BOBAMANLY <jacoblfisher36@gmail.com>
2022-03-23 15:01:19 -0600

    changed menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import arcade
+import arcade.gui
 import threading
 
 from WordBlasters.wbmain import WordBlasters
@@ -17,25 +18,47 @@ class MainMenu(arcade.Window):
     """
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        # game_view = 
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+        self.v_box = arcade.gui.UIBoxLayout()
         
+        math_button = arcade.gui.UIFlatButton(text="Oasis", width=200)
+        self.v_box.add(math_button.with_space_around(bottom=20))
+
+        word_button = arcade.gui.UIFlatButton(text="WordBlasters", width=200)
+        self.v_box.add(word_button.with_space_around(bottom=20))
+
+        hangman_button = arcade.gui.UIFlatButton(text="Hangman", width=200)
+        self.v_box.add(hangman_button.with_space_around(bottom=20))
+
+        word_button.on_click = MainMenu.run_word_blasters
+        hangman_button.on_click = MainMenu.run_hangman
+        # game_view = 
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x = "center_x",
+                anchor_y = "center_y",
+                child = self.v_box)
+        )
+        
+    @staticmethod
+    def run_word_blasters(event):
+        wb = threading.Thread(target = WordBlasters().run)
+        wb.start()
+    @staticmethod
+    def run_hangman(event):
+        hm = threading.Thread(target = Hangman_game().run)
+        hm.start()
 
     def on_key_press(self, symbol, modifier):
         print(symbol)
 
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
+
 def main():
-    print("What game do you want to run? ")
-    print("1. Word Blasters")
-    print("2. Hangman")
-    print("3. Oasis")
     window = MainMenu()
-    wb = threading.Thread(target = WordBlasters().run)
-    wb.start()
-
-    hm = threading.Thread(target = Hangman_game().run)
-    hm.start()
-
     # window.show_view(game_view)
     arcade.run()
 main()
-
